c37776d652bd0bd64b1e92dabcd65e43a279d51c
Itsaadarsh <heyitsaadarsh@gmail.com>
2021-07-26 15:36:34 +0530

    Add user auth middlerware, cookies, logout functionality

--- controller/authController.go
+++ controller/authController.go
@@ -3,7 +3,10 @@ package controller
 import (
 	"onestep/database"
 	"onestep/models"
+	"strconv"
+	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gofiber/fiber/v2"
 	"golang.org/x/crypto/bcrypt"
 )
@@ -60,8 +63,69 @@ func Login(c *fiber.Ctx) error {
 			"message": "incorrect password",
 		})
 	}
+
+	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
+		Issuer:    strconv.Itoa(int(user.ID)),
+		ExpiresAt: time.Now().Add(time.Hour * 24).Unix(),
+	})
+
+	token, err := claims.SignedString([]byte("secret"))
+	if err != nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"message": "something went wrong",
+		})
+	}
+
+	cookie := fiber.Cookie{
+		Name:     "jwt",
+		Value:    token,
+		Expires:  time.Now().Add(time.Hour * 24),
+		HTTPOnly: true,
+	}
+
+	c.Cookie(&cookie)
+
 	return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
 		"message": "successful login",
 	})
 
+}
+
+type Claims struct {
+	jwt.StandardClaims
+}
+
+func User(c *fiber.Ctx) error {
+	cookie := c.Cookies("jwt")
+	token, err := jwt.ParseWithClaims(cookie, &Claims{}, func(t *jwt.Token) (interface{}, error) {
+		return []byte("secret"), nil
+	})
+
+	if err != nil || !token.Valid {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"message": "unauthorized",
+		})
+	}
+
+	claims := token.Claims.(*Claims)
+
+	var user models.User
+
+	database.DB.First(&user, claims.Issuer)
+
+	return c.Status(fiber.StatusOK).JSON(user)
+}
+
+func Logout(c *fiber.Ctx) error {
+	cookie := fiber.Cookie{
+		Name:     "jwt",
+		Value:    "",
+		Expires:  time.Now().Add(-time.Hour),
+		HTTPOnly: true,
+	}
+
+	c.Cookie(&cookie)
+	return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+		"message": "successful logout",
+	})
 }
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/alexbrainman/sspi v0.0.0-20180613141037-e580b900e9f5 // indirect
 	github.com/andybalholm/brotli v1.0.3 // indirect
 	github.com/cespare/reflex v0.3.0 // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/gofiber/fiber/v2 v2.15.0 // indirect
 	github.com/jackc/pgx/v4 v4.13.0 // indirect
 	github.com/jcmturner/gokrb5/v8 v8.2.0 // indirect
